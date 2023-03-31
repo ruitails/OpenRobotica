0c356f576fdf48e3ccbc1927df20ade30eb457b3
Rui Teixeira <ruiteixeira737@gmail.com>
2023-03-31 03:13:50 +0100

    Added movements functions

---
+++ src/movements.c
@@ -0,0 +1,58 @@
+//Source file for all movement functions
+#include <Arduino.h>
+#include "movements.h"
+
+//Move forward
+void frente(){
+
+}
+
+//Move backwards 
+void tras(void){
+
+}
+
+//Move right
+void direita(void){
+
+}
+
+//Move left
+void esquerda(void){
+
+}
+
+//Move clockwise
+void horario(void){
+
+}
+
+//Move anti-clockwise
+void anti(void){
+
+}
+
+//Move front-right
+void cima_direita(void){
+
+}
+
+//Move front-left
+void cima_esquerda(void){
+
+}
+
+//Move back-right
+void baixo_direita(){
+
+}
+
+//Move back-left
+void baixo_esquerda(){
+
+}
+
+//Stop
+void stop(){
+
+}
